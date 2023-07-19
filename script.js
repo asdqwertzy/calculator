275f82ac98673461dac8d87c3f1ba60c2f2e8eff
asdqwertzy <asdf666@mail.com>
2023-07-19 12:51:59 +0200

    Add a thousands-seperator functionality; injecting commas every 3 digits in the input field

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ var inputF = document.querySelector("input")
 
 inputF.addEventListener("input", function(event) {
     event.target.value = inputF.value.replace(/\D/g, "");
+    var thousandsSeperator = inputF.value.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    event.target.value = thousandsSeperator;
 }); 
 
