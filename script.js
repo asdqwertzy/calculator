89d062f3a9b6dd72493e7bec613636462a011b5d
asdqwertzy <asdf666@mail.com>
2023-07-20 08:32:34 +0200

    Add button:active styling, ESC and Backspace functionality

--- script.js
+++ script.js
@@ -2,17 +2,24 @@ var inputF = document.querySelector("input")
 
 
 
-inputF.addEventListener("input", function(event) {
+inputF.addEventListener("input", function (event) {
     event.target.value = inputF.value.replace(/\D/g, "");
     var thousandsSeperator = inputF.value.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     event.target.value = thousandsSeperator;
-}); 
+});
 
 
-document.addEventListener("keydown", function(event) {
+document.addEventListener("keydown", function (event) {
     if (event.key.match(/[0-9]/)) {
         if (document.activeElement !== inputF) {
             inputF.focus()
-            }
+        }
+    }
+    if (event.key == "Escape") {
+        inputF.value = ""
+    }
+    if (event.key == "Backspace") {
+        inputF.focus()
+
     }
 })
